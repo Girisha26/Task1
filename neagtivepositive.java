34a3141141521431931cbbc20782242e77e88776
Girisha M V <110905339+Girisha26@users.noreply.github.com>
2024-01-29 13:34:56 +0530

    Update neagtivepositive.java

--- neagtivepositive.java
+++ neagtivepositive.java
@@ -3,13 +3,13 @@ public class neagtivepositive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int a = sc.nextInt();
+        int a = sc.nextInt();//getiing input and storing at a 
 
-        if(a >= 0)
+        if(a >= 0)//excutes if a is greater or equal to 0
         {
             System.out.println("Result : " + a +" is a positive number");
         }
-        else
+        else//exectues esle part if a is lesser than 0
         {
             System.out.println("Result : " + a +" is a neagtive number");
 
