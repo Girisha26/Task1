2f5972b6ee619286867ad77f5281484ad90b700b
Girisha M V <110905339+Girisha26@users.noreply.github.com>
2024-01-29 13:33:13 +0530

    Update range.java

--- range.java
+++ range.java
@@ -6,20 +6,20 @@ public class range
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Starting point:");
-        int x = sc.nextInt();
+        int x = sc.nextInt();//Getting input and storing at x
 
         System.out.print("Ending point:");
-        int y = sc.nextInt();
+        int y = sc.nextInt();//Getting input and storing at y
 
         System.out.println("______________________");
         System.out.println("NUMBERS FROM " + x +" TO "+ y);
         System.out.println("______________________");
 
-
+        //loop to traverse from x to y 
         for(int i=x ; i<=y ; i++)
         {
-            System.out.println(x);
-            x++;
+            System.out.println(x);//prints x 
+            x++;//To increament the c=value of x 
         }
 
 
