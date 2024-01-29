4b3dfe4bd5d4ecd43954947b123592f401a13860
Girisha M V <110905339+Girisha26@users.noreply.github.com>
2024-01-29 13:51:58 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -2,11 +2,11 @@ public class pattern
 {
     public static void main(String[] args)
     {
-        for (int i = 5; i > 0; i--)
+        for (int i = 5; i > 0; i--)//outer loop for rows
         {
-            for (int j = 5; j > 0; j--)
+            for (int j = 5; j > 0; j--)//inner loop for columns
             {
-                if(j>i)
+                if(j>i)//checking current column is j or not 
                     System.out.print(" " + j);
                 else {
                     System.out.print(" " + i);
